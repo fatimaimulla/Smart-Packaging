c9bcc602c47d9c89d0254d015d04dff9596b901b
Khan Tufailahmed <tufailahmedkhan708@gmail.com>
2026-01-29 00:40:30 +0530

    testing

--- Client/src/api/updateSideDimension.js
+++ Client/src/api/updateSideDimension.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 export const updateSideDimension = async ({ sideView, sessionId }) => {
-  const res = await axios.post(`${baseURL}api/img/updateside`, {
+  const res = await axios.post(`${baseURL}/api/img/updateside`, {
     sideView,
     sessionId,
   });
